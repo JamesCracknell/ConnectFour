a4b6a8df42e1f62eca7feedd530afd010e146072
James Cracknell <71817624+JamesCracknell@users.noreply.github.com>
2021-03-07 13:42:25 +0000

    Add files via upload

---
+++ NEA_OOP/NEA_OOP/Player_Vs_Player_Menu.vb
@@ -0,0 +1,277 @@
+﻿Public Class Player_Vs_Player_Menu
+
+    ' Control Declarations
+
+    Private Title As New PictureBox
+    Private CreditLabel As New Label
+    Private Shadows BackgroundImage As New PictureBox
+    Private PlayerChoiceLabel As New Label
+    Private InputPlayerNameLabel As New Label
+    Private PlayerNameTextBox As New TextBox
+    Private SubmitPlayerOneNameButton As New Button
+    Private SubmitPlayerTwoNameButton As New Button
+    Private TimerOptionButton As New Button 'MAKE THIS A VISUAL TOGGLE
+    Private TimerOptionLabel As New Label
+    Private TimerExplanationLabel As New Label
+    Private PlayerOneCurrentNameLabel As New Label
+    Private PlayerTwoCurrentNameLabel As New Label
+    Private IncorrectInputLabel As New Label
+    Private StartingPlayerLabel As New Label
+    Private StartingPlayerButton As New Button
+    Private ColourToggleDescriptionLabel As New Label
+    Private ColourToggleButton As New Button
+    Private CanContinueIndicatorLabel As New Label
+    Private ContinueButton As New Button
+    Private TimeCustomiserTextbox As New TextBox
+    Private TimeCustomiserLabel As New Label
+
+    ' Variable Declarations
+    Private FirstTimeRunning As Boolean = True ' some code only needs to run when it is not the first time 
+
+    ' Main Code
+    Public Sub This_Form_Setup()
+        Me.Show()
+        If FirstTimeRunning Then
+            Processes.Form_Setup(Me)
+            ' Form Title
+            Title.Location = New Point(242, 5)
+            Title.Size = New Size(500, 50)
+            Title.SizeMode = PictureBoxSizeMode.Zoom
+            Title.Image = Image.FromFile("Title.png")
+            Controls.Add(Title)
+            Processes.RunTimeContructor(CreditLabel, 0, 748, 984, 13, "James Cracknell - 191673", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 8.25, Cursors.Default, Color.Transparent)
+            Controls.Add(CreditLabel)
+            ' Background Image
+            BackgroundImage = New PictureBox
+            BackgroundImage.Location = New Point(242, 100)
+            BackgroundImage.Size = New Size(500, 500)
+            BackgroundImage.SizeMode = PictureBoxSizeMode.Zoom 'sets size mode to allow the whole image to be shown
+            BackgroundImage.Image = Image.FromFile("BackgroundImage.png")
+            Controls.Add(BackgroundImage)
+            FirstTimeRunning = False
+            Menu_Setup()
+        Else
+            PlayerOneCurrentNameLabel.Text = ""
+            PlayerTwoCurrentNameLabel.Text = ""
+            TimeCustomiserTextbox.Text = 120
+            SubmitPlayerOneNameButton.BackColor = Color.Transparent
+            SubmitPlayerTwoNameButton.BackColor = Color.Transparent
+        End If
+    End Sub
+    Public Sub Menu_Setup()
+        ' PlayerChoiceLabel
+        Processes.RunTimeContructor(PlayerChoiceLabel, 0, 50, 500, 20, "Customise settings for the game:", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 12, Cursors.Default, Color.Transparent)
+        Controls.Add(PlayerChoiceLabel)
+        PlayerChoiceLabel.Parent = BackgroundImage 'sets the label's parent to the background image 
+        PlayerChoiceLabel.Refresh() 'refreshes the label, to take on its new properties
+
+        ' InputPlayerNameLabel
+        Processes.RunTimeContructor(InputPlayerNameLabel, 0, 70, 500, 20, "Enter player names below:", ContentAlignment.TopCenter, "Microsoft Sans Serif", 10.25, Cursors.Default, Color.Transparent)
+        Controls.Add(InputPlayerNameLabel)
+        InputPlayerNameLabel.Parent = BackgroundImage 'sets the label's parent to the background image 
+        InputPlayerNameLabel.Refresh()
+
+        ' PlayerNameTextBox
+        Processes.RunTimeContructor(PlayerNameTextBox, 342, 190, 300, 50, "", ContentAlignment.TopCenter, "Microsoft Sans Serif", 9.75, Cursors.Hand, Color.Empty) 'color.empty is a null colour, allowing for controls that do not allow customised back colours
+        Controls.Add(PlayerNameTextBox)
+
+        ' SubmitPlayerOneNameButton
+        Processes.RunTimeContructor(SubmitPlayerOneNameButton, 362, 220, 120, 50, "Submit Player One Name", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9.75, Cursors.Hand, Color.Transparent)
+        AddHandler SubmitPlayerOneNameButton.Click, AddressOf SubmitPlayerNameButton_Click
+        SubmitPlayerOneNameButton.Name = "SubmitPlayerOneNameButton"
+        Controls.Add(SubmitPlayerOneNameButton)
+
+        ' PlayerOneCurrentNameLabel
+        Processes.RunTimeContructor(PlayerOneCurrentNameLabel, 113, 165, 132, 20, "", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9, Cursors.Default, Color.Transparent)
+        Controls.Add(PlayerOneCurrentNameLabel)
+        PlayerOneCurrentNameLabel.Parent = BackgroundImage 'sets the label's parent to the background image 
+        PlayerOneCurrentNameLabel.Refresh()
+
+        ' SubmitPlayerTwoNameButton
+        Processes.RunTimeContructor(SubmitPlayerTwoNameButton, 502, 220, 120, 50, "Submit Player Two Name", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9.75, Cursors.Hand, Color.Transparent)
+        AddHandler SubmitPlayerTwoNameButton.Click, AddressOf SubmitPlayerNameButton_Click
+        SubmitPlayerTwoNameButton.Name = "SubmitPlayerTwoNameButton"
+        Controls.Add(SubmitPlayerTwoNameButton)
+
+        ' PlayerTwoCurrentNameLabel
+        Processes.RunTimeContructor(PlayerTwoCurrentNameLabel, 253, 165, 132, 20, "", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9, Cursors.Default, Color.Transparent)
+        Controls.Add(PlayerTwoCurrentNameLabel)
+        PlayerTwoCurrentNameLabel.Parent = BackgroundImage 'sets the label's parent to the background image 
+        PlayerTwoCurrentNameLabel.Refresh()
+
+        ' IncorrectInputLabel
+        Processes.RunTimeContructor(IncorrectInputLabel, 342, 285, 300, 22, "", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9, Cursors.Default, Color.IndianRed)
+        Controls.Add(IncorrectInputLabel)
+        IncorrectInputLabel.Visible = False
+
+        ' TimerOptionLabel
+        Processes.RunTimeContructor(TimerOptionLabel, 100, 205, 300, 20, "Would you like to add a timer to the game?", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 10.25, Cursors.Default, Color.Transparent)
+        Controls.Add(TimerOptionLabel)
+        TimerOptionLabel.Parent = BackgroundImage 'sets the label's parent to the background image 
+        TimerOptionLabel.Refresh()
+
+        ' TimerOptionButton
+        Processes.RunTimeContructor(TimerOptionButton, 392, 327, 200, 30, "Timed Game", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9.75, Cursors.Hand, Color.Transparent)
+        AddHandler TimerOptionButton.Click, AddressOf TimerOptionButton_Click
+        Controls.Add(TimerOptionButton)
+
+        ' TimerExplanationLabel
+        Processes.RunTimeContructor(TimerExplanationLabel, 25, 255, 450, 40, "Timed Game: The game is timed." & vbNewLine & "In the event of a draw, the player with the lowest time wins.", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9, Cursors.Default, Color.Transparent)
+        Controls.Add(TimerExplanationLabel)
+        TimerExplanationLabel.Parent = BackgroundImage 'sets the label's parent to the background image 
+        TimerExplanationLabel.Refresh()
+
+        ' TimeCustomiserLabel
+        Processes.RunTimeContructor(TimeCustomiserLabel, 745, 320, 225, 40, "Enter the time to count down from:" & vbNewLine & "(in seconds)", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 10.25, Cursors.Default, Color.Transparent)
+        TimeCustomiserLabel.Visible = False
+        Controls.Add(TimeCustomiserLabel)
+
+        ' TimeCustomiserTextbox
+        Processes.RunTimeContructor(TimeCustomiserTextbox, 755, 360, 205, 30, "120", ContentAlignment.TopCenter, "Microsoft Sans Serif", 9.75, Cursors.Hand, Color.Empty)
+        AddHandler TimeCustomiserTextbox.Click, AddressOf TimerOptionButton_Click
+        TimeCustomiserTextbox.Visible = False
+        Controls.Add(TimeCustomiserTextbox)
+
+        ' StartingPlayerLabel
+        Processes.RunTimeContructor(StartingPlayerLabel, 25, 280, 450, 40, "Choose the starting player:", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 10.25, Cursors.Default, Color.Transparent)
+        Controls.Add(StartingPlayerLabel)
+        StartingPlayerLabel.Parent = BackgroundImage 'sets the label's parent to the background image
+        StartingPlayerLabel.Refresh()
+
+        ' StartingPlayerButton
+        Processes.RunTimeContructor(StartingPlayerButton, 392, 415, 200, 30, "Player One", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 10.25, Cursors.Hand, Color.Transparent)
+        AddHandler StartingPlayerButton.Click, AddressOf StartingPlayerButton_Click
+        Controls.Add(StartingPlayerButton)
+
+        ' ColourToggleDescriptionLabel
+        Processes.RunTimeContructor(ColourToggleDescriptionLabel, 25, 335, 450, 40, "Choose player one's colour:", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 10.25, Cursors.Default, Color.Transparent)
+        Controls.Add(ColourToggleDescriptionLabel)
+        ColourToggleDescriptionLabel.Parent = BackgroundImage 'sets the label's parent to the background image
+        ColourToggleDescriptionLabel.Refresh()
+
+        ' ColourToggleButton
+        Processes.RunTimeContructor(ColourToggleButton, 392, 470, 200, 30, "Red", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 10.25, Cursors.Hand, Color.Transparent)
+        ColourToggleButton.ForeColor = Color.Red
+        AddHandler ColourToggleButton.Click, AddressOf ColourToggleButton_Click
+        Controls.Add(ColourToggleButton)
+
+        ' ContinueButton
+        Processes.RunTimeContructor(ContinueButton, 392, 505, 200, 30, "Start Game" & vbNewLine & "In the event of a draw, the player with the lowest time wins.", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 10.25, Cursors.Hand, Color.Transparent)
+        ContinueButton.Font = New Font(ContinueButton.Font, FontStyle.Bold)
+        AddHandler ContinueButton.Click, AddressOf ContinueButton_Click
+        Controls.Add(ContinueButton)
+
+        ' CanContinueIndicatorLabel
+        Processes.RunTimeContructor(CanContinueIndicatorLabel, 392, 527, 200, 30, "Please enter both usernames.", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9, Cursors.Default, Color.IndianRed)
+        Controls.Add(CanContinueIndicatorLabel)
+        CanContinueIndicatorLabel.Visible = False
+        BackgroundImage.SendToBack() 'sends the image to the back so that all text and buttons appear in front of it
+    End Sub
+    Public Sub SubmitPlayerNameButton_Click(sender As Object, e As EventArgs)
+        CanContinueIndicatorLabel.Visible = False
+        CanContinueIndicatorLabel.Refresh()
+        If SubmitPlayerOneNameButton.BackColor = Color.IndianRed Then SubmitPlayerOneNameButton.BackColor = Color.Transparent 'if the colour is red, resets it
+        If SubmitPlayerTwoNameButton.BackColor = Color.IndianRed Then SubmitPlayerTwoNameButton.BackColor = Color.Transparent 'if the colour is red, resets it
+        IncorrectInputLabel.Visible = False
+        NameVerification()
+        If NameVerification() Then 'if the input was valid
+            sender.backcolor = Color.LightGreen
+            If sender.name = "SubmitPlayerOneNameButton" Then
+                PlayerOneCurrentNameLabel.Text = PlayerNameTextBox.Text
+            Else
+                PlayerTwoCurrentNameLabel.Text = PlayerNameTextBox.Text
+            End If
+            PlayerNameTextBox.Text = ""
+        Else 'invalid input
+            IncorrectInputLabel.Visible = True
+            sender.backcolor = Color.IndianRed
+            PlayerNameTextBox.Text = ""
+        End If
+    End Sub
+    Public Function NameVerification()
+        IncorrectInputLabel.Text = "INVALID INPUT: Name must be 1-16 characters" 'reset default invalid input message
+        Dim valid As Boolean = True
+        If PlayerNameTextBox.Text = "" Then valid = False 'input can't be blank
+        If PlayerNameTextBox.Text.Length > 16 Then valid = False 'input can't be longer than 16 characters
+        If (PlayerNameTextBox.Text = PlayerTwoCurrentNameLabel.Text Or PlayerNameTextBox.Text = PlayerOneCurrentNameLabel.Text) And PlayerNameTextBox.Text <> "" Then
+            valid = False
+            IncorrectInputLabel.Text = "INVALID INPUT: Names must not be the same" 'set special invalid input message
+        End If
+        Return valid
+    End Function
+    Public Sub TimerOptionButton_Click(sender As Object, e As EventArgs)
+        If sender.text = "Timed Game" Then
+            TimeCustomiserLabel.Visible = True
+            TimeCustomiserTextbox.Visible = True
+            TimerOptionButton.Text = "Countdown"
+            TimerExplanationLabel.Text = "Countdown: Set a timer for each player that counts down during their turn." & vbNewLine & "If the time runs out on your turn then you lose!"
+        ElseIf sender.text = "Countdown" Then
+            TimeCustomiserLabel.Visible = False
+            TimeCustomiserTextbox.Visible = False
+            TimerOptionButton.Text = "No Timer"
+            TimerExplanationLabel.Text = "No Timer:  There is no timer." & vbNewLine & "A draw is a draw!"
+        ElseIf sender.text = "No Timer" Then
+            TimerOptionButton.Text = "Timed Game"
+            TimerExplanationLabel.Text = "Timed Game: The game is timed." & vbNewLine & "In the event of a draw, the player with the lowest time wins."
+        End If
+    End Sub
+    Public Sub ColourToggleButton_Click(sender As Object, e As EventArgs)
+        If sender.Text = "Red" Then
+            ColourToggleButton.Text = "Yellow"
+            ColourToggleButton.ForeColor = Color.Yellow
+        Else
+            ColourToggleButton.Text = "Red"
+            ColourToggleButton.ForeColor = Color.Red
+        End If
+    End Sub
+    Public Sub StartingPlayerButton_Click(sender As Object, e As EventArgs)
+        If sender.Text = "Player One" Then
+            StartingPlayerButton.Text = "Player Two"
+        ElseIf sender.Text = "Player Two" Then
+            StartingPlayerButton.Text = "Random"
+        Else
+            StartingPlayerButton.Text = "Player One"
+        End If
+    End Sub
+    Public Function getOppositeColour(ColourChoice As String)
+        If ColourChoice = "Red" Then
+            Return "Yellow"
+        Else
+            Return "Red"
+        End If
+    End Function
+    Public Sub ContinueButton_Click(sender As Object, e As EventArgs)
+        If CanContinue() Then
+            CanContinueIndicatorLabel.Visible = False
+            Me.Hide()
+            Dim SubType As String = TimerOptionButton.Text
+            Dim ColourChoice As String = ColourToggleButton.Text
+            Dim OppositeColour As String = getOppositeColour(ColourChoice)
+            Dim StartingPlayer As String
+            If StartingPlayerButton.Text = "Player One" Then
+                StartingPlayer = PlayerOneCurrentNameLabel.Text
+            ElseIf StartingPlayerButton.Text = "Player Two" Then
+                StartingPlayer = PlayerTwoCurrentNameLabel.Text
+            Else
+                If (CInt(Math.Floor(Rnd() * 2))) = 0 Then 'random number, either 1 or 0 resulting in 50% chance to be either player
+                    StartingPlayer = PlayerOneCurrentNameLabel.Text
+                Else
+                    StartingPlayer = PlayerTwoCurrentNameLabel.Text
+                End If
+            End If
+            Game_Form.Game_Setup("Player", SubType, PlayerOneCurrentNameLabel.Text, ColourChoice, PlayerTwoCurrentNameLabel.Text, OppositeColour, StartingPlayer, TimeCustomiserTextbox.Text)
+        Else
+            CanContinueIndicatorLabel.Visible = True
+        End If
+    End Sub
+    Private Function CanContinue() 'determines if the user has performed the necessary steps to continue
+        If PlayerOneCurrentNameLabel.Text <> "" And PlayerTwoCurrentNameLabel.Text <> "" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Private Sub Player_Vs_Player_Menu_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing 'when x is pressed
+        Main_Menu.Close() 'closing first form closes whole program
+    End Sub
+End Class
---
+++ NEA_OOP/NEA_OOP/Previous_Game_States.vb
@@ -0,0 +1,227 @@
+﻿Imports System.IO
+Public Class Previous_Game_States
+    ' Control Declarations
+
+    Private Title As New PictureBox
+    Private BoardLocations(6, 5) As PictureBox
+    Private CloseButton As New Button
+    Private GameTypeTitleLabel As New Label
+    Private GameTypeLabel As New Label
+    Private SubTypeTitleLabel As New Label
+    Private SubTypeLabel As New Label
+    Private WinnerNameTitleLabel As New Label
+    Private WinnerNameLabel As New Label
+    Private LoserNameTitleLabel As New Label
+    Private LoserNameLabel As New Label
+    Private PlayerOneTimeTitleLabel As New Label
+    Private PlayerOneTimeLabel As New Label
+    Private PlayerTwoTimeTitleLabel As New Label
+    Private PlayerTwoTimeLabel As New Label
+
+    ' Variable Declarations
+
+    Protected OneDBoardState(42) As String
+    Protected BoardState(6, 5) As String
+    Private Painted As Boolean = False
+    ' Main Code
+    Public Sub This_Form_Setup(DesiredGame)
+        Me.Show()
+        Painted = False 'so the board is reprinted at the start
+        Processes.Form_Setup(Me)
+        Me.Height = 900
+        ' Form Title
+        Title.Location = New Point(242, 5)
+        Title.Size = New Size(500, 50)
+        Title.SizeMode = PictureBoxSizeMode.Zoom
+        Title.Image = Image.FromFile("Title.png")
+        Controls.Add(Title)
+        Board_Setup()
+        ' CloseButton
+        Processes.RunTimeContructor(CloseButton, 342, 780, 300, 50, "Close", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9.75, Cursors.Hand, Color.Transparent)
+        AddHandler CloseButton.Click, AddressOf CloseButton_Click
+        Controls.Add(CloseButton)
+        Load_Previous_Game_State(DesiredGame)
+        UpdateBoard()
+        SetUpExtraInfo(DesiredGame)
+        AddHandler Me.Paint, AddressOf Board_Paint
+    End Sub
+
+    Sub Load_Previous_Game_State(DesiredGame)
+        Dim LatestGameState As String
+        Using reader As New StreamReader("PreviousGameStates.txt") 'saves requested row in file to variable
+            For i = 0 To DesiredGame 'moves down file until user specified line
+                LatestGameState = reader.ReadLine 'saves the row 
+            Next
+        End Using
+        Convert1DArrayTo2DArray(LatestGameState)
+    End Sub
+    Public Function GetExtraInfo(DesiredGame)
+        Dim ExtraInfo As String
+        Dim ExtraInfoArray() As String
+        Using reader As New StreamReader("PreviousGames.txt")
+            For i = 0 To DesiredGame 'moves down file until user specified line
+                extrainfo = reader.ReadLine
+            Next
+        End Using
+        ExtraInfoArray = Split(ExtraInfo, ",")
+        Return ExtraInfoArray
+    End Function
+
+    Public Sub SetUpExtraInfo(DesiredGame)
+        ' Close Button
+        Processes.RunTimeContructor(CloseButton, 342, 780, 300, 50, "Close", ContentAlignment.MiddleCenter, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        Dim ExtraInfo As String()
+        ExtraInfo = GetExtraInfo(DesiredGame)
+        ' GameTypeTitleLabel
+        Processes.RunTimeContructor(GameTypeTitleLabel, 150, 95, 125, 20, "Game Type:", ContentAlignment.BottomLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        GameTypeTitleLabel.Font = New Font(GameTypeTitleLabel.Font, FontStyle.Bold)
+        Controls.Add(GameTypeTitleLabel)
+        ' GameTypeLabel
+        Processes.RunTimeContructor(GameTypeLabel, 150, 100, 125, 50, ExtraInfo(1), ContentAlignment.MiddleLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        Controls.Add(GameTypeLabel)
+        ' SubTypeTitleLabel
+        Processes.RunTimeContructor(SubTypeTitleLabel, 275, 95, 130, 20, "Subtype:", ContentAlignment.BottomLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        SubTypeTitleLabel.Font = New Font(SubTypeTitleLabel.Font, FontStyle.Bold)
+        Controls.Add(SubTypeTitleLabel)
+        ' SubTypeLabel
+        Processes.RunTimeContructor(SubTypeLabel, 275, 100, 130, 50, ExtraInfo(2), ContentAlignment.MiddleLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        Controls.Add(SubTypeLabel)
+        ' WinnerNameTitleLabel
+        Processes.RunTimeContructor(WinnerNameTitleLabel, 405, 95, 75, 20, "Winner:", ContentAlignment.BottomLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        WinnerNameTitleLabel.Font = New Font(WinnerNameTitleLabel.Font, FontStyle.Bold)
+        Controls.Add(WinnerNameTitleLabel)
+        ' WinnerNameLabel
+        Processes.RunTimeContructor(WinnerNameLabel, 405, 100, 115, 50, ExtraInfo(6), ContentAlignment.MiddleLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        Controls.Add(WinnerNameLabel)
+        ' LoserNameTitleLabel
+        Processes.RunTimeContructor(LoserNameTitleLabel, 520, 95, 115, 20, "Loser:", ContentAlignment.BottomLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        LoserNameTitleLabel.Font = New Font(LoserNameTitleLabel.Font, FontStyle.Bold)
+        Controls.Add(LoserNameTitleLabel)
+        ' LoserNameLabel
+        If ExtraInfo(6) = ExtraInfo(3) Then 'winner is player1
+            Processes.RunTimeContructor(LoserNameLabel, 520, 100, 115, 50, ExtraInfo(4), ContentAlignment.MiddleLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        Else
+            Processes.RunTimeContructor(LoserNameLabel, 520, 100, 115, 50, ExtraInfo(3), ContentAlignment.MiddleLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+        End If
+        Controls.Add(LoserNameLabel)
+        If ExtraInfo(2) = "Countdown" Or ExtraInfo(2) = "Timed Game" Then 'if the game had a timer
+            'PlayerOneTimeTitleLabel
+            Processes.RunTimeContructor(PlayerOneTimeTitleLabel, 635, 95, 175, 20, "Player One Time:", ContentAlignment.BottomLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+            PlayerOneTimeTitleLabel.Font = New Font(PlayerOneTimeTitleLabel.Font, FontStyle.Bold)
+            Controls.Add(PlayerOneTimeTitleLabel)
+            ' PlayerOneTimeLabel
+            Processes.RunTimeContructor(PlayerOneTimeLabel, 635, 100, 175, 50, ExtraInfo(7), ContentAlignment.MiddleLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+            Controls.Add(PlayerOneTimeLabel)
+            'PlayerTwoTimeTitleLabel
+            Processes.RunTimeContructor(PlayerTwoTimeTitleLabel, 810, 95, 175, 20, "Player Two Time:", ContentAlignment.BottomLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+            PlayerTwoTimeTitleLabel.Font = New Font(PlayerTwoTimeTitleLabel.Font, FontStyle.Bold)
+            Controls.Add(PlayerTwoTimeTitleLabel)
+            ' PlayerTwoTimeLabel
+            Processes.RunTimeContructor(PlayerTwoTimeLabel, 810, 100, 175, 50, ExtraInfo(8), ContentAlignment.MiddleLeft, "Microsoft Sans Serif", 9.75, Cursors.Default, Color.Transparent)
+            Controls.Add(PlayerTwoTimeLabel)
+            If ExtraInfo(2) = "Countdown" Then 'if it is a countdown game, the title is changed
+                PlayerOneTimeTitleLabel.Text = "Player One Time Left:"
+                PlayerTwoTimeTitleLabel.Text = "Player Two Time Left:"
+                PlayerOneTimeTitleLabel.Refresh()
+                PlayerTwoTimeTitleLabel.Refresh()
+            End If
+        End If
+
+    End Sub
+    Sub Convert1DArrayTo2DArray(LatestGameState) 'Converts grid to 2D array
+        OneDBoardState = Split(LatestGameState, ",")
+        For x = 0 To 6
+            For y = 0 To 5
+                If OneDBoardState(x * 6 + y) = "0" Then
+                    BoardState(x, y) = "0"
+                ElseIf OneDBoardState(x * 6 + y) = "R" Then
+                    BoardState(x, y) = "R"
+                ElseIf OneDBoardState(x * 6 + y) = "Y" Then
+                    BoardState(x, y) = "Y"
+                ElseIf OneDBoardState(x * 6 + y) = "WR" Then
+                    BoardState(x, y) = "WR"
+                ElseIf OneDBoardState(x * 6 + y) = "WY" Then
+                    BoardState(x, y) = "WY"
+                End If
+            Next
+        Next
+    End Sub
+    Public Sub Board_Setup()
+        Dim XPos, YPos As Integer
+        ' Board
+        XPos = 146
+        For x = 0 To 6
+            YPos = 170
+            For y = 0 To 5
+                BoardLocations(x, y) = New PictureBox
+                BoardLocations(x, y) = New PictureBox
+                BoardLocations(x, y).Location = New Point(XPos, YPos)
+                BoardLocations(x, y).Size = New Size(98, 98)
+                BoardLocations(x, y).SizeMode = PictureBoxSizeMode.Zoom ' Set to fully display image without warping it
+                BoardLocations(x, y).BackColor = Color.Blue 'since I cannot make it transparent, I made the background blue to match the box
+                BoardLocations(x, y).Enabled = False
+                BoardLocations(x, y).Tag = (x * 6 + y)
+                Controls.Add(BoardLocations(x, y))
+                YPos += 100
+            Next
+            XPos += 100
+        Next
+    End Sub
+
+    Private Sub Board_Paint(sender As Object, e As PaintEventArgs)
+        If Painted = False Then 'prevents the board being redrawn. It should only be drawn once.
+            Painted = True
+            Dim g As Graphics
+            g = Me.CreateGraphics
+
+            ' Rectangle Background
+            Dim myBrush As New SolidBrush(Color.Blue) 'creates a new blue brush 
+            Dim formGraphics As Graphics
+            formGraphics = Me.CreateGraphics()
+            formGraphics.FillRectangle(myBrush, New Rectangle(145, 170, 700, 600))
+            myBrush.Dispose()
+            formGraphics.Dispose()
+            Dim BlackPen As New Pen(Brushes.Black)
+            BlackPen.Width = 6.0F
+            ' Rectangle Outline
+            g.DrawRectangle(BlackPen, 145, 170, 700, 600) '(pentype, startingx, startingy, xsize, ysize)
+
+            'vertical lines
+            Dim x1 As Integer = 245
+            For i = 0 To 6
+                g.DrawLine(BlackPen, x1, 170, x1, 770) '(pentype, startingx, startingy, endingx, endingy)
+                x1 += 100
+            Next
+
+            'horizontal lines
+            Dim y1 As Integer = 270
+            For j = 0 To 5
+                g.DrawLine(BlackPen, 145, y1, 845, y1)
+                y1 += 100
+            Next
+        End If
+    End Sub
+
+    Public Sub UpdateBoard()
+        For x = 0 To 6
+            For y = 0 To 5
+                If BoardState(x, y) = "Y" Then
+                    BoardLocations(x, y).Image = Image.FromFile("YellowTokenTransparent.png")
+                ElseIf BoardState(x, y) = "R" Then
+                    BoardLocations(x, y).Image = Image.FromFile("RedTokenTransparent.png")
+                ElseIf BoardState(x, y) = "WY" Then
+                    BoardLocations(x, y).Image = Image.FromFile("YellowToken.png")
+                ElseIf BoardState(x, y) = "WR" Then
+                    BoardLocations(x, y).Image = Image.FromFile("RedToken.png")
+                Else
+                    BoardLocations(x, y).Image = Nothing 'if an image is there that shouldn't be there it is cleared (such as when the game is reset)
+                End If
+            Next
+        Next
+    End Sub
+
+
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) 'if CloseButton is clicked
+        Me.Close()
+    End Sub
+End Class
